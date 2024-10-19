579fa410a98375c8e74651bf1a531b5c1430e1f9
Pedro Martineli <martinelipedro@gmail.com>
2024-10-19 16:59:07 -0300

    feat: config app routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,10 +27,9 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name='index' options={{headerShown: false}}></Stack.Screen>
       </Stack>
     </ThemeProvider>
   );
---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const screen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Tesasdasdasdasdte</Text>
+            <Text>Tesasdasdasdasdte</Text>
+            <Text>Tesasdasdasdasdte</Text>
+            <Text>Tesasdasdasdasdte</Text>
+
+            <Text>Tesasdasdasdasdte</Text>
+            <Text>Tesasdasdasdasdte</Text>
+            <Text>Tesasdasdasdasdte</Text>
+            <Text>Tesasdasdasdasdte</Text>
+            <Text>Tesasdasdasdasdte</Text>
+
+        </SafeAreaView>
+    )
+}
+
+export default screen;
